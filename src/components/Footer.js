2b89b6f5c0727863b48fcf471bab6b57c0790572
Daulet Sabyr <daulet.sabyr@gmail.com>
2018-01-22 09:12:08 +0600

    сайттың ресми атауын түзедім

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import LocalizedStrings from 'react-localization';
+import {ru, kk} from '../languages/header.json';
+
+let e = new LocalizedStrings({ru,kk});
 
 export default class Footer extends React.Component {
+	constructor() {
+    super();
+	    (localStorage.getItem('lang')) ? e.setLanguage(localStorage.getItem('lang')) : e.setLanguage('ru');
+  	}
 	render(){
 		return(
 			<footer>
 				<div className="container">
-	                <p>&copy; 2017 - <strong>Управление архитектуры и градостроительства города Алматы</strong></p>
+	                <p>&copy; 2017 - <strong>{e.copyright}</strong></p>
 	            </div>
 	        </footer>
 		)
