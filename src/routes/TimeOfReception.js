dc4c01e231d7f3476b2a28ef733ef63a783817cd
Nurseit <nurseit_sharip@mail.ru>
2018-02-02 15:57:58 +0600

    Кабылдау уакыттары

--- src/routes/TimeOfReception.js
+++ src/routes/TimeOfReception.js
@@ -29,26 +29,33 @@ export default class timeOfReception extends React.Component{
                 </div>
 
 
+
                 <table class="table table-striped">
-                    <tbody>
+
+                    <tbody style={{width: '1000px', paddingTop: '0px'}}>
+                    <h2 class="captionline" colspan="5">{e.captionline}</h2>
+                    <hr/>
 
                         <thead>
-                            <tr className="captionline"></tr>
-                            <td colspan="5">{e.captionline}</td>
+
 
 
                     <tr>
-                        <th scope="row">#</th>
-                        <th>{e.col_1_1}</th>
-                        <th>{e.col_1_2}</th>
-                        <th>{e.col_1_3}</th>
-                        <th>{e.col_1_4}</th>
+                        <td scope="row"></td>
+                        <td style={{width: '250px' }}><strong>{e.col_1_1}</strong></td>
+                        <td style={{width: '260px' }}><strong>{e.col_1_2}</strong></td>
+                        <td></td>
+                        <td style={{width: '240px' }}><strong>{e.col_1_3}</strong></td>
+                        <td><strong>{e.col_1_4}</strong></td>
                     </tr>
 
                     <tr scope="row">
-                        <th>1</th>
+                        <td><strong>1</strong></td>
                         <td>{e.col_2_1}</td>
                         <td>{e.col_2_2}</td>
+                        <th>
+                            <img style={{width: '', height: '', display: "inline"}} src="/images/architects/zhanbyrshy.png"/>
+                        </th>
                         <td>{e.col_2_3}
                             <br/>
                             {e.col_2_3_2}</td>
@@ -59,9 +66,12 @@ export default class timeOfReception extends React.Component{
                     </tr>
 
                     <tr scope="row">
-                        <th>2</th>
+                        <td><strong>2</strong></td>
                         <td>{e.col_3_1}</td>
                         <td>{e.col_3_2}</td>
+                        <th>
+                            <img style={{width: '', height: '', display: "inline"}} src="/images/architects/tolepbergen.png"/>
+                        </th>
                         <td>{e.col_3_3}
                             <br/>
                             {e.col_3_3_2}</td>
@@ -72,9 +82,12 @@ export default class timeOfReception extends React.Component{
                     </tr>
 
                     <tr scope="row">
-                        <th>3</th>
+                        <td><strong>3</strong></td>
                         <td>{e.col_4_1}</td>
                         <td>{e.col_4_2}</td>
+                        <th>
+                            <img style={{width: '', height: '', display: "inline"}} src="/images/architects/rymzhanov.png"/>
+                        </th>
                         <td>{e.col_4_3}
                             <br/>
                             {e.col_4_3_2}</td>
@@ -85,9 +98,12 @@ export default class timeOfReception extends React.Component{
                     </tr>
 
                     <tr scope="row">
-                        <th>4</th>
+                        <td><strong>4</strong></td>
                         <td>{e.col_5_1}</td>
                         <td>{e.col_5_2}</td>
+                        <th>
+                            <img style={{width: '', height: '', display: "inline"}} src="/images/architects/isaev.png"/>
+                        </th>
                         <td>{e.col_5_3}
                             <br/>
                             {e.col_5_3_2}</td>
@@ -98,9 +114,12 @@ export default class timeOfReception extends React.Component{
                     </tr>
 
                     <tr scope="row">
-                        <th>5</th>
+                        <td><strong>5</strong></td>
                         <td>{e.col_6_1}</td>
                         <td>{e.col_6_2}</td>
+                        <th>
+                            <img style={{width: '', height: '', display: "inline"}} src="/images/architects/kultasov.png"/>
+                        </th>
                         <td>{e.col_6_3}
                             <br/>
                             {e.col_6_3_2}</td>
@@ -111,9 +130,12 @@ export default class timeOfReception extends React.Component{
                     </tr>
 
                     <tr scope="row">
-                        <th>6</th>
+                        <td><strong>6</strong></td>
                         <td>{e.col_7_1}</td>
                         <td>{e.col_7_2}</td>
+                        <th>
+                            <img style={{width: '', height: '', display: "inline"}} src="/images/architects/zhumabekov.png"/>
+                        </th>
                         <td>{e.col_7_3}
                             <br/>
                             {e.col_7_3_2}</td>
@@ -124,9 +146,12 @@ export default class timeOfReception extends React.Component{
                     </tr>
 
                     <tr scope="row">
-                        <th>7</th>
+                        <td><strong>7</strong></td>
                         <td>{e.col_8_1}</td>
                         <td>{e.col_8_2}</td>
+                        <th>
+                            <img style={{width: '', height: '', display: "inline"}} src="/images/architects/myrzekov.png"/>
+                        </th>
                         <td>{e.col_8_3}
                             <br/>
                             {e.col_8_3_2}</td>
@@ -137,9 +162,12 @@ export default class timeOfReception extends React.Component{
                     </tr>
 
                     <tr scope="row">
-                        <th>8</th>
+                        <td><strong>8</strong></td>
                         <td>{e.col_9_1}</td>
                         <td>{e.col_9_2}</td>
+                        <th>
+                            <img style={{width: '', height: '', display: "inline"}} src="/images/architects/sembaev.png"/>
+                        </th>
                         <td>{e.col_9_3}
                             <br/>
                             {e.col_9_3_2}</td>
