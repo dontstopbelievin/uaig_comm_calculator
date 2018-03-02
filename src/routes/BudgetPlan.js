f6954b72012ef80e3fdcd261b02c0773ecc3192c
Zhalgas <1994-23@mail.ru>
2018-03-02 13:33:59 +0600

    Добавил АПИ для страницы "Бюджетное планирование"

--- src/routes/BudgetPlan.js
+++ src/routes/BudgetPlan.js
@@ -22,7 +22,7 @@ export default class BudgetPlan extends React.Component {
   getFiles() {
     var token = sessionStorage.getItem('tokenInfo');
     var xhr = new XMLHttpRequest();
-    xhr.open("get", window.url + "api/File/system_category/budget", true);
+    xhr.open("get", window.url + "api/system_files/category/budget", true);
       xhr.setRequestHeader("Authorization", "Bearer " + token);
       xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
       xhr.onload = function() {
@@ -34,48 +34,58 @@ export default class BudgetPlan extends React.Component {
   }
 
   // Скачивание файла
-  downloadFile(event) {
-    var id =  event.target.getAttribute("data-id");
+  downloadFile(id) {
     var xhr = new XMLHttpRequest();
-    xhr.open("get", window.url + 'api/file/download/systemfile/budget/' + id, true);
+    xhr.open("get", window.url + 'api/system_files/download/budget/' + id, true);
       xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
       xhr.onload = function() {
         if (xhr.status === 200) {
-          var data = JSON.parse(xhr.responseText);
-          var base64ToArrayBuffer = (function () {
+          //test of IE
+          if (typeof window.navigator.msSaveBlob === "function") {
+            window.navigator.msSaveBlob(xhr.response, "tc-" + new Date().getTime() + ".pdf");
+          } else {
+            var data = JSON.parse(xhr.responseText);
+            var today = new Date();
+            var curr_date = today.getDate();
+            var curr_month = today.getMonth() + 1;
+            var curr_year = today.getFullYear();
+            var formated_date = "(" + curr_date + "-" + curr_month + "-" + curr_year + ")";
+
+            var base64ToArrayBuffer = (function () {
         
-            return function (base64) {
-              var binaryString =  window.atob(base64);
-              var binaryLen = binaryString.length;
-              var bytes = new Uint8Array(binaryLen);
-              
-              for (var i = 0; i < binaryLen; i++) {
-                var ascii = binaryString.charCodeAt(i);
-                bytes[i] = ascii;
+              return function (base64) {
+                var binaryString =  window.atob(base64);
+                var binaryLen = binaryString.length;
+                var bytes = new Uint8Array(binaryLen);
+                
+                for (var i = 0; i < binaryLen; i++) {
+                  var ascii = binaryString.charCodeAt(i);
+                  bytes[i] = ascii;
+                }
+                
+                return bytes; 
               }
               
-              return bytes; 
-            }
-            
-          }());
+            }());
 
-          var saveByteArray = (function () {
-            var a = document.createElement("a");
-            document.body.appendChild(a);
-            a.style = "display: none";
-            
-            return function (data, name) {
-              var blob = new Blob(data, {type: "octet/stream"}),
-                  url = window.URL.createObjectURL(blob);
-              a.href = url;
-              a.download = name;
-              a.click();
-              window.URL.revokeObjectURL(url);
-            };
+            var saveByteArray = (function () {
+              var a = document.createElement("a");
+              document.body.appendChild(a);
+              a.style = "display: none";
+              
+              return function (data, name) {
+                var blob = new Blob(data, {type: "octet/stream"}),
+                    url = window.URL.createObjectURL(blob);
+                a.href = url;
+                a.download = name;
+                a.click();
+                window.URL.revokeObjectURL(url);
+              };
 
-          }());
+            }());
 
-          saveByteArray([base64ToArrayBuffer(data.byteFile)], data.fileName + data.fileExt);
+            saveByteArray([base64ToArrayBuffer(data.file)], data.file_name);
+          }
         } else {
           alert('Не удалось скачать файл');
         }
@@ -85,35 +95,33 @@ export default class BudgetPlan extends React.Component {
 
   render() {
     return (
-        <div>
-
-
-      <div className="content container budget-plan-page">
-        <div className="card">
-          <div className="card-header">
-          <h4 className="mb-0">Бюджетное планирование</h4></div>
-          <div className="card-body">
-            <ul>
-            {this.state.files.map(function(file, index){
-              return(
-                <a key={index} className="list-group-item" >
-                  <i className="glyphicon glyphicon-file"></i>
-                  <div className="bmd-list-group-col">
-                    <p className="list-group-item-heading pointer" data-id={file.Id} onClick={this.downloadFile.bind(this)}>
-                      {file.Name}
-                    </p>
-                    <p className="list-group-item-text">{file.Description}</p>
-                  </div>
-                  <i className="glyphicon glyphicon-download-alt pull-xs-right pointer" data-id={file.Id} onClick={this.downloadFile.bind(this)}></i>
-                </a>
-                );
-              }.bind(this))
-            }
-            </ul>
+      <div>
+        <div className="content container budget-plan-page">
+          <div className="card">
+            <div className="card-header">
+            <h4 className="mb-0">Бюджетное планирование</h4></div>
+            <div className="card-body">
+              <ul>
+              {this.state.files.map(function(file, index){
+                return(
+                  <a key={index} className="list-group-item" >
+                    <i className="glyphicon glyphicon-file"></i>
+                    <div className="bmd-list-group-col">
+                      <p className="list-group-item-heading pointer" onClick={this.downloadFile.bind(this, file.id)}>
+                        {file.name}
+                      </p>
+                      <p className="list-group-item-text">{file.description}</p>
+                    </div>
+                    <i className="glyphicon glyphicon-download-alt pull-xs-right pointer" onClick={this.downloadFile.bind(this, file.id)}></i>
+                  </a>
+                  );
+                }.bind(this))
+              }
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-        </div>
     )
   }
 }
