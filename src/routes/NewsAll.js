142e66e57d760b8d23dcc4f7a0d26a1abf1f7d9b
Dinmukhamed <bossgenius54@gmail.com>
2018-07-23 15:04:02 +0600

    Добавлен прелоадер для новостей

--- src/routes/NewsAll.js
+++ src/routes/NewsAll.js
@@ -11,43 +11,54 @@ let e = new LocalizedStrings({ru,kk});
 
 export default class newsAll extends React.Component{
 
-    constructor() {
-        super();
-        (localStorage.getItem('lang')) ? e.setLanguage(localStorage.getItem('lang')) : e.setLanguage('ru');
+  constructor() {
+      super();
+      (localStorage.getItem('lang')) ? e.setLanguage(localStorage.getItem('lang')) : e.setLanguage('ru');
 
-        this.state = {
-            tokenExists: false,
-        };
-    }
+      this.state = {
+          tokenExists: false,
+      };
+  }
 
 
-    render() {
-        return(
-            
-            <div className="container body-content">
-                    <div className="row">
-                        <div className="col-md-12 col-xs-12 black-main text-center">
-                            <h4 >ВСЕ НОВОСТИ</h4>
-                            <span><img src="/images/line.png" /></span>
-                            
+  render() {
+    return(
 
-                                <div className="card mt-8 mb-8 wow fadeInLeft" data-wow-duration="1.5s">
+      <div className="container body-content">
+        <div className="row">
+          <div className="col-md-12 col-xs-12 black-main text-center">
+            <h4 >ВСЕ НОВОСТИ</h4>
+            <span>
+              <img src="/images/line.png" />
+            </span>
 
-                                    <div className="list-group">
-                                        <Switch>
-                                          <Route path="/newsAll/status/:status" component={AllNews} />
-                                          <Redirect from="/newsAll" to="/newsAll/status/1" />
-                                        </Switch>
-                                    </div>
 
-                                </div>
-                        </div>
-                    </div>
-                    <br/>
-                    <br/>
+            <div className="card mt-8 mb-8 wow fadeInTop" data-wow-duration="1.5s">
+              <div className="list-group">
+
+                <div className="row">
+
+                  <div className="col-sm-5 statusActive">
+                    <ul className="nav nav-tabs mb-2 pull-right">
+                      <li className="nav-item"><NavLink exact activeClassName="nav-link active" className="nav-link"  to="/newsAll/status/1" replace>Новости управления</NavLink></li>
+                      <li className="nav-item"><NavLink exact activeClassName="nav-link active" className="nav-link"  to="/newsAll/status/2" replace>СМИ о нас</NavLink></li>
+                      <li className="nav-item"><NavLink activeClassName="nav-link active" className="nav-link"  to="/newsAll/status/3" replace>Кадры решают все</NavLink></li>
+                    </ul>
+                  </div>
                 </div>
-        )
-    }
+                  <Switch>
+                    <Route path="/newsAll/status/:status" component={AllNews} />
+                    <Redirect from="/newsAll" to="/newsAll/status/1" />
+                  </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br/>
+        <br/>
+      </div>
+    )
+  }
 }
 
 class AllNews extends React.Component {
@@ -62,64 +73,65 @@ class AllNews extends React.Component {
     }
 
     componentDidMount() {
-         new WOW.WOW({
-            live: false
-        }).init();
-        this.getNews();
+      new WOW.WOW({
+        live: false
+      }).init();
+      this.getNews();
       }
 
 
     componentWillReceiveProps(nextProps) {
-        if(this.props.match.params.status !== nextProps.match.params.status) {
-          this.getNews(nextProps.match.params.status);
-        }
+      if(this.props.match.params.status !== nextProps.match.params.status) {
+        this.getNews(nextProps.match.params.status);
       }
+    }
 
     getNews (status = null) {
 
-        if (!status) {
-          status = this.props.match.params.status;
-          console.log(status);
-        }
-        var link = 'api/news/all';
-        var xhr = new XMLHttpRequest();
-        xhr.open("get", window.url + link, true);
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-        xhr.onload = function() {
-          if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            console.log(data.news);
-            var x = data.news.length;
+      if (!status) {
+        status = this.props.match.params.status;
+        console.log(status);
+      }
+      this.setState({loaderHidden:false});
+      var link = 'api/news/all';
+      var xhr = new XMLHttpRequest();
+      xhr.open("get", window.url + link, true);
+      xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var data = JSON.parse(xhr.responseText);
+          console.log(data.news);
+          var x = data.news.length;
 
-            for (var i = 0; i < x ; i ++ ){
-                data.news[i].created_at = data.news[i].created_at.substr(0,10);
-            }
-            var news = '';
-            switch (status) {
-              case '1':
-                news = data.news.filter(function(obj) { return obj.heading_id === 1 });
-                break;
-
-              case '2':
-                news = data.news.filter(function(obj) { return obj.heading_id === 2; });
-                break;
-
-              case '3':
-                news = data.news.filter(function(obj) { return obj.heading_id === 3; });
-                break;
-
-              default:
-                news = data.news;
-                break;
-            }
-
-            this.setState({news: news});
-            this.setState({loaderHidden: true});
-          } else {
-            alert("Записи новостей не удалось найти в базе данных!");
+          for (var i = 0; i < x ; i ++ ){
+              data.news[i].created_at = data.news[i].created_at.substr(0,10);
           }
-        }.bind(this);
-        xhr.send();
+          var news = '';
+          switch (status) {
+            case '1':
+              news = data.news.filter(function(obj) { return obj.heading_id === 1 });
+              break;
+
+            case '2':
+              news = data.news.filter(function(obj) { return obj.heading_id === 2; });
+              break;
+
+            case '3':
+              news = data.news.filter(function(obj) { return obj.heading_id === 3; });
+              break;
+
+            default:
+              news = data.news;
+              break;
+          }
+
+          this.setState({news: news});
+          this.setState({loaderHidden: true});
+        } else {
+          alert("Записи новостей не удалось найти в базе данных!");
+        }
+      }.bind(this);
+      xhr.send();
     }
 
 
@@ -130,42 +142,32 @@ class AllNews extends React.Component {
       <div>
         {this.state.loaderHidden &&
           <div>
-            <div className="row">
-
-              <div className="col-sm-5 statusActive">
-                <ul className="nav nav-tabs mb-2 pull-right">
-                  <li className="nav-item"><NavLink exact activeClassName="nav-link active" className="nav-link"  to="/newsAll/status/1" replace>Новости управления</NavLink></li>
-                  <li className="nav-item"><NavLink exact activeClassName="nav-link active" className="nav-link"  to="/newsAll/status/2" replace>СМИ о нас</NavLink></li>
-                  <li className="nav-item"><NavLink activeClassName="nav-link active" className="nav-link"  to="/newsAll/status/3" replace>Кадры решают все</NavLink></li>
-                </ul>
-              </div>
-            </div>
-                {this.state.news.map(function (article) {
-                  var link = '/#/NewsArticle/' + article.id;
-                  return (
-                    <div className="row">
-                      <div className="col-md-1"></div>
-                      <div href="#" className="list-group-item col-md-10 flex-column align-items-start  wow fadeInDown"
-                           data-wow-duration="1.5s">
-                        <div className="d-flex w-100 left-content-between">
-                          <img className="news-icon" src="./images/clock-icon.png" alt="Время выхода"/>
-                          <p className="news-date text-muted font-weight-light">{article.created_at}</p>
-                        </div>
-
-                        <h6 className="news-title text-left text-muted mb-1">{article.title}</h6>
-                        <p className="news-text text-left mt-2 mb-1">{article.description}</p>
-                        <div className="dropdown-divider"></div>
-                        <a href={link}>
-                          <small className="float-right text-warning view-more font-weight-bold">Читать далее</small>
-                        </a>
-                        <br />
-                        <hr />
-                      </div>
+            {this.state.news.map(function (article) {
+              var link = '/#/NewsArticle/' + article.id;
+              return (
+                <div className="row">
+                  <div className="col-md-1"></div>
+                  <div href="#" className="list-group-item col-md-10 flex-column align-items-start  wow fadeInDown"
+                       data-wow-duration="1.5s">
+                    <div className="d-flex w-100 left-content-between">
+                      <img className="news-icon" src="./images/clock-icon.png" alt="Время выхода"/>
+                      <p className="news-date text-muted font-weight-light">{article.created_at}</p>
                     </div>
-                  );
 
-                }.bind(this))
-                }
+                    <h6 className="news-title text-left text-muted mb-1">{article.title}</h6>
+                    <p className="news-text text-left mt-2 mb-1">{article.description}</p>
+                    <div className="dropdown-divider"></div>
+                    <a href={link}>
+                      <small className="float-right text-warning view-more font-weight-bold">Читать далее</small>
+                    </a>
+                    <br />
+                    <hr />
+                  </div>
+                </div>
+              );
+
+            }.bind(this))
+            }
           </div>
         }
         {!this.state.loaderHidden &&
