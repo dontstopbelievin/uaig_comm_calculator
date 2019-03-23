0cf7097098cda2c010eac1ce56c00d93a3a3675f
aman <amanbek_94@mail.ru>
2019-03-23 15:48:27 +0600

    A

--- src/routes/apz/citizen/ShowMap.js
+++ src/routes/apz/citizen/ShowMap.js
@@ -23,8 +23,10 @@ export default class ShowMap extends React.Component {
     this.props.changeFunction(data);
   }
 
-  saveCoordinates() {
+  saveCoordinates(address) {
     this.changeState('projectAddressCoordinates', $('#coordinates').html());
+
+    this.changeState('projectAddress', address);
 
     this.props.hasCoordinates(true);
 
@@ -40,18 +42,19 @@ export default class ShowMap extends React.Component {
 
     var oldPoint = [];
     var withPoint = this.props.point;
+    var address = '';
     var coordinates = this.props.coordinates;
 
     return (
       <div>
         {withPoint ?
           <div className="row">
-            <div className="col-sm-6">
+            <div className="col-sm-4">
               <h5 className="block-title-2 mt-0 mb-3">Карта</h5>
             </div>
-            <div className="col-sm-6">
+            <div className="col-sm-8">
               <div className="pull-right">
-                <button type="button" className="btn btn-outline-success mr-1" onClick={() => this.saveCoordinates()}>Сохранить</button>
+                <button type="button" className="btn btn-outline-success mr-1" onClick={() => this.saveCoordinates(address)}>Сохранить местоположение</button>
                 <button type="button" className="btn btn-outline-secondary" onClick={this.toggleMap.bind(this, false)}>Закрыть карту</button>
               </div>
             </div>
@@ -60,13 +63,11 @@ export default class ShowMap extends React.Component {
           <h5 className="block-title-2 mt-5 mb-3">Карта</h5>
         }
         <div id="coordinates" style={{display: 'none'}}></div>
-        <div className="col-md-12 viewDiv">
+        <div id="viewDivCrusor" className="col-md-12 viewDiv">
           <EsriLoaderReact options={options}
             modulesToLoad={[
               'esri/views/MapView',
-
               'esri/widgets/LayerList',
-
               'esri/WebScene',
               'esri/layers/FeatureLayer',
               'esri/layers/TileLayer',
@@ -78,11 +79,17 @@ export default class ShowMap extends React.Component {
               "esri/layers/MapImageLayer",
               'esri/Graphic',
               'esri/config',
+              "esri/tasks/IdentifyTask",
+              "esri/tasks/support/IdentifyParameters",
+              "esri/tasks/FindTask",
+              "esri/tasks/support/FindParameters",
               'dojo/domReady!'
             ]}
 
             onReady={({loadedModules: [MapView, LayerList, WebScene, FeatureLayer, TileLayer, Search, WebMap, webMercatorUtils, dom, Map,
-              MapImageLayer, Graphic, esriConfig], containerNode}) => {
+              MapImageLayer, Graphic, esriConfig, IdentifyTask, IdentifyParameters, FindTask, FindParameters], containerNode}) => {
+
+              var identifyTask, params;
               esriConfig.portalUrl = "https://gis.uaig.kz/arcgis";
               var map = new WebMap({
                 basemap: "streets",
@@ -90,17 +97,6 @@ export default class ShowMap extends React.Component {
                   id: "b5a3c97bd18442c1949ba5aefc4c1835"
                 }
               });
-              /*var map = new Map("map",{
-                scale: 250000,
-                maxScale: 500,
-                minScale: 250000,
-                slider:false
-              });
-              var layer, layer2;
-              layer = new MapImageLayer("http://gis.uaig.kz/server/rest/services/Map2d/Базовая_карта_MIL1/MapServer");
-              layer2 = new MapImageLayer("http://gis.uaig.kz/server/rest/services/Map2d/объекты_города/MapServer");
-              map.layers.add(layer2);
-              map.layers.add(layer);*/
 
               if (coordinates) {
                 var coordinatesArray = coordinates.split(", ");
@@ -142,11 +138,84 @@ export default class ShowMap extends React.Component {
                 });
               }
 
+              if(this.props.kadastr_number){
+                var find = new FindTask({
+                  url: "https://gis.uaig.kz/server/rest/services/Map2d/объекты_города3/MapServer"
+                });
+                var params = new FindParameters({
+                  layerIds: [27, 14],
+                  searchFields: ['cadastre_n', 'cadastre_number'],
+                  returnGeometry: true
+                });
+                params.searchText = this.props.kadastr_number;
+                find.execute(params).then(showResults);
+                function showResults(response) {
+                  return response.results.map(function(result) {
+                    switch (result.layerName) {
+                      case 'Здания и сооружения':
+                          console.log(result.feature.attributes['полный адрес'].trim());
+                          address = result.feature.attributes['полный адрес'].trim();
+                          showPoint(result.feature.geometry.centroid.latitude, result.feature.geometry.centroid.longitude);
+                          break;
+                      case 'Акт приемки':
+                          console.log(result.feature.attributes['street_nam'].trim());
+                          address = result.feature.attributes['street_nam'].trim();
+                          showPoint(result.feature.geometry.centroid.latitude, result.feature.geometry.centroid.longitude);
+                          break;
+                      default:
+                          console.log('Не найден');
+                          address = 'Не найден';
+                    }
+                    return address;
+                  });
+                }
+                function showPoint(latitude, longitude){
+                  view.center = [longitude.toFixed(5), latitude.toFixed(5)];
+                  dom.byId("coordinates").innerHTML = longitude.toFixed(5) + ", " + latitude.toFixed(5);
+
+                  var point = {
+                    type: "point",
+                    longitude: longitude.toFixed(5),
+                    latitude: latitude.toFixed(5)
+                  };
+
+                  var markerSymbol = {
+                    type: "simple-marker",
+                    color: [226, 119, 40],
+                    outline: {
+                      color: [255, 255, 255],
+                      width: 2
+                    }
+                  };
+
+                  var pointGraphic = new Graphic({
+                    geometry: point,
+                    symbol: markerSymbol
+                  });
+
+                  view.graphics.remove(oldPoint);
+                  view.graphics.add(pointGraphic);
+
+                  oldPoint = pointGraphic;
+                }
+              }
+
               if (withPoint) {
                 view.on("click", showCoordinates);
 
-                function showCoordinates(evt) {
-                  var mp = webMercatorUtils.webMercatorToGeographic(evt.mapPoint);
+                // Create identify task for the specified map service
+                identifyTask = new IdentifyTask('https://gis.uaig.kz/server/rest/services/Map2d/объекты_города3/MapServer');
+
+                // Параметры для поиска
+                params = new IdentifyParameters();
+                params.tolerance = 3; //Дистанция от точки клика в пикселях
+                params.layerIds = [14]; //Номера слоев где искать
+                params.layerOption = "top"; // искать на верхних слоях (IdentifyParameters.LAYER_OPTION_VISIBLE;)
+                params.width = view.width; // Размеры видимой карты
+                params.height = view.height;
+
+                function showCoordinates(event) {
+                  var mp = webMercatorUtils.webMercatorToGeographic(event.mapPoint);
                   dom.byId("coordinates").innerHTML = mp.x.toFixed(5) + ", " + mp.y.toFixed(5);
 
                   var point = {
@@ -173,6 +242,29 @@ export default class ShowMap extends React.Component {
                   view.graphics.add(pointGraphic);
 
                   oldPoint = pointGraphic;
+
+                  // Set the geometry to the location of the view click
+                  params.geometry = event.mapPoint;
+                  params.mapExtent = view.extent;
+
+                  identifyTask.execute(params).then(function(response) {
+
+                    var results = response.results;
+
+                    return results.map(function(result) {
+
+                      var feature = result.feature;
+                      var layerName = result.layerName;
+
+                      feature.attributes.layerName = layerName;
+                      if (layerName === 'Здания и сооружения') {
+                        // console.log(feature.attributes['полный адрес'].trim());
+                        address = feature.attributes['полный адрес'].trim();
+                      }
+                      return feature;
+                    });
+                  });
+
                 }
               }
 
@@ -180,33 +272,6 @@ export default class ShowMap extends React.Component {
                 view: view,
                 sources: [{
                   featureLayer: new FeatureLayer({
-                    //url: "https://gis.uaig.kz/server/rest/services/Hosted/%D0%97%D0%B0%D1%80%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5_%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5_%D0%B0%D0%BA%D1%82%D1%8B/FeatureServer",
-                    url: "https://gis.uaig.kz/server/rest/services/Map2d/объекты_города3/MapServer/14",
-                    popupTemplate: { // autocasts as new PopupTemplate()
-                      title: `<table>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);"><td class="attrName">Кадастровый номер:</td>  <td class="attrValue">{cadastre_number}</td></tr>
-                        <tr style="background-color: rgba(0, 255, 0, 0.05);"><td class="attrName">Код административного района:</td>  <td class="attrValue">{id_admraiona}</td></tr>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);"><td class="attrName">Адрес:</td>  <td class="attrValue">{address}</td></tr>
-                        <tr style="background-color: rgba(0, 255, 0, 0.05);"><td class="attrName">Наименование</td>  <td class="attrValue">{name}</td></tr>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);"><td class="attrName">Общая площадь:</td>  <td class="attrValue">{obsch_area}</td></tr>
-                        <tr style="background-color: rgba(0, 255, 0, 0.05);"><td class="attrName">Площадь жил. помещения:</td>  <td class="attrValue">{zhil_area}</td></tr>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);"><td class="attrName">Год постройки:</td>  <td class="attrValue">{year_of_foundation}</td></tr>
-                      </table>`
-                    }
-                  }),
-                  searchFields: ["kad_n"],
-                  displayField: "kad_n",
-                  exactMatch: false,
-                  outFields: ["*"],
-                  name: "Кадастровый номер",
-                  placeholder: "введите кадастровый номер",
-                  maxResults: 6,
-                  maxSuggestions: 6,
-                  enableSuggestions: true,
-                  minCharacters: 0
-                },{
-                  featureLayer: new FeatureLayer({
-                    //url: "https://gis.uaig.kz/server/rest/services/Hosted/%D0%97%D0%B0%D1%80%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5_%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5_%D0%B0%D0%BA%D1%82%D1%8B/FeatureServer",
                     url: "https://gis.uaig.kz/server/rest/services/Map2d/объекты_города3/MapServer/27",
                     popupTemplate: { // autocasts as new PopupTemplate()
                       title: `<table>
@@ -224,36 +289,7 @@ export default class ShowMap extends React.Component {
                   exactMatch: false,
                   outFields: ["*"],
                   name: "Кадастровый номер(Акт приемки)",
-                  placeholder: "введите кадастровый номер",
-                  maxResults: 6,
-                  maxSuggestions: 6,
-                  enableSuggestions: true,
-                  minCharacters: 0
-                },
-                {
-                  featureLayer: new FeatureLayer({
-                    url: "https://gis.uaig.kz/server/rest/services/Map2d/объекты_города3/MapServer/14",
-                    popupTemplate: {
-                      title: `<table>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);width:100%"><td class="attrName">Адресный массив:</td>  <td class="attrValue">{id_adr_massive}</td></tr>
-                        <tr style="background-color: rgba(0, 255, 0, 0.05);"><td class="attrName">Количество этажей:</td>  <td class="attrValue">{floor}</td></tr>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);"><td class="attrName">Год постройки:</td>  <td class="attrValue">{year_of_foundation}</td></tr>
-                        <tr style="background-color: rgba(0, 255, 0, 0.05);"><td class="attrName">Общая площадь:</td>  <td class="attrValue">{obsch_area}</td></tr>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);"><td class="attrName">Объем здания, м3:</td>  <td class="attrValue">{volume_build}</td></tr>
-                        <tr style="background-color: rgba(0, 255, 0, 0.05);"><td class="attrName">Площадь жил. помещения:</td>  <td class="attrValue">{zhil_area}</td></tr>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);"><td class="attrName">Площадь застройки, м2:</td>  <td class="attrValue">{zastr_area}</td></tr>
-                        <tr style="background-color: rgba(0, 255, 0, 0.05);"><td class="attrName">Наименование первичной улицы:</td>  <td class="attrValue">{street_name_1}</td></tr>
-                        <tr style="background-color: rgba(0, 0, 255, 0.05);"><td class="attrName">Основной номер дома:</td>  <td class="attrValue">{number_1}</td></tr>
-                        <tr style="background-color: rgba(0, 255, 0, 0.05);"><td class="attrName">Второстепенный номер дома:</td>  <td class="attrValue">{number_2}</td></tr>
-                      </table>`
-                    }
-                  }),
-                  searchFields: ["street_name_1","id_adr_massive", "address"],
-                  displayField: "street_name_1" ,
-                  exactMatch: false,
-                  outFields: ["*"],
-                  name: "Здания и сооружения",
-                  placeholder: "введите улицу",
+                  placeholder: "Введите кадастровый номер",
                   maxResults: 6,
                   maxSuggestions: 6,
                   enableSuggestions: true,
