f5a605c3e9f548a11dbcc35a3c93b76f46065a21
dospayevtair <tair.dospayev@gmail.com>
2019-04-23 17:38:57 +0600

    tair's changes

--- src/routes/Actions.js
+++ src/routes/Actions.js
@@ -326,10 +326,10 @@ export default class CitizenActions extends React.Component {
                 </div>
                 <div className="apzinfo-bottom">
                     <div className="card-button">
-                    {/*<button type="button" onClick={this.InProcess} className="btn btn-secondary">Перейти к заявке</button>*/}
-                    {this.state.tokenExists && this.state.rolename === 'Citizen' && <NavLink to={"/panel/citizen/religbuilding/status/active/1"} replace className="btn btn-primary">Подать заявку</NavLink>}
-                    {this.state.tokenExists && this.state.rolename === 'Region' && <NavLink to={"/panel/urban/religbuilding/status/active/1"} replace className="btn btn-primary">Просмотр заявок</NavLink>}
-                    {this.state.tokenExists && this.state.rolename === 'Head' && <NavLink to={"/panel/head/religbuilding/status/active/1"} replace className="btn btn-primary">Просмотр заявок</NavLink>}
+                    <button type="button" onClick={this.InProcess} className="btn btn-secondary">Перейти к заявке</button>
+                    {/*{this.state.tokenExists && this.state.rolename === 'Citizen' && <NavLink to={"/panel/citizen/religbuilding/status/active/1"} replace className="btn btn-primary">Подать заявку</NavLink>}*/}
+                    {/*{this.state.tokenExists && this.state.rolename === 'Region' && <NavLink to={"/panel/urban/religbuilding/status/active/1"} replace className="btn btn-primary">Просмотр заявок</NavLink>}*/}
+                    {/*{this.state.tokenExists && this.state.rolename === 'Head' && <NavLink to={"/panel/head/religbuilding/status/active/1"} replace className="btn btn-primary">Просмотр заявок</NavLink>}*/}
                     <div className="reglament">
                         <a href="http://adilet.zan.kz/rus/docs/V14S0002879">Регламент Гос. услуги</a>
                         <a href="http://adilet.zan.kz/rus/docs/V1500011183">Стандарт Гос. услуги</a>
